393cc3fa9a0194bf0bbe6f10ddfd3a9911a38393
Hunter <jenningsdhunter@gmail.com>
2019-03-31 12:37:01 -0400

    Fixing lamda function

--- src/js/modules/importData.js
+++ src/js/modules/importData.js
@@ -1,5 +1,5 @@
 export default async function getTracks() {
-  // return fetch('/.netlify/functions/getTracks').then(response => response.json());
-  // ! Change back to netlify function after development
-  return fetch('http://localhost:9000/getTracks').then(response => response.json());
+  return fetch('/.netlify/functions/getTracks').then(response => response.json());
+  // For Development
+  // return fetch('http://localhost:9000/getTracks').then(response => response.json());
 }
