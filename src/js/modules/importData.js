91c72ee71e310de1007c96adf22a6ab2ab61e9db
Hunter <jenningsdhunter@gmail.com>
2019-03-31 12:31:08 -0400

    Making player mobile responsive and adding the new mobile elements to the player logic where needed

--- src/js/modules/importData.js
+++ src/js/modules/importData.js
@@ -1,3 +1,5 @@
 export default async function getTracks() {
-  return fetch('/.netlify/functions/getTracks').then(response => response.json());
+  // return fetch('/.netlify/functions/getTracks').then(response => response.json());
+  // ! Change back to netlify function after development
+  return fetch('http://localhost:9000/getTracks').then(response => response.json());
 }
